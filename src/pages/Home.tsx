5758d9d7f33f699de93d122ca689ee3e972d7a76
yukicoder0509 <bendoris0509@gmail.com>
2025-08-26 15:23:30 +0800

    Add get comment and post request back

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Navigation from "@/components/Navigation";
 import Button from "@/components/Button.tsx";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { useNavigate } from "react-router";
-import { getPosts } from "@/requests/getPosts.tsx";
+import { getPosts } from "@/requests/getPosts.ts";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
