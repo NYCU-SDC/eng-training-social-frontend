af5f3cc32be063e3b9556935ec8c1289e0dc101b
yukicoder0509 <bendoris0509@gmail.com>
2025-09-29 20:43:16 +0800

    feat: complete text part for PostCard component

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,15 @@
+import "./PostCard.css";
+
+export default function PostCard() {
+  return (
+    <div className="card-container">
+      <h1 className="card-title">Lorem ipsum dolor sit amet consectetur.</h1>
+      <p className="card-text">
+        Lorem ipsum dolor sit amet consectetur. Bibendum adipiscing metus dolor
+        diam in risus ut. Quis nam elit placerat egestas tellus rhoncus.
+        Facilisis pretium et nunc metus urna nisi a. Molestie consequat ornare
+        ac volutpat velit condimentum quis pellentesque.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import PostCard from "@/components/PostCard.tsx";
+
+export default function Home() {
+  return (
+    <div>
+      <PostCard />
+    </div>
+  );
+}
