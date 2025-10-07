8bc82057ae78ee7113184bb7119fe8afa46d7138
yukicoder0509 <bendoris0509@gmail.com>
2025-10-07 18:53:18 +0800

    feat: add navigation  function to new post button in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,19 @@ import PostCard from "@/components/PostCard.tsx";
 import Navigation from "@/components/Navigation.tsx";
 import Button from "@/components/Button.tsx";
 import { PlusIcon } from "@heroicons/react/24/solid";
+import { useNavigate } from "react-router";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="main-container">
       <div className="content-scroll">
         <div className="button-container">
-          <Button className="new-post-button">
+          <Button
+            className="new-post-button"
+            onClick={() => navigate("/post/create")}
+          >
             <PlusIcon className="md-icon" />
             <p>New Post</p>
           </Button>
