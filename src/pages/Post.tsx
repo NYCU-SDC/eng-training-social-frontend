ec3ec46579d19c81eeef078689fee27e716892d8
yukicoder0509 <bendoris0509@gmail.com>
2025-10-07 20:28:16 +0800

    feat: add input bar and go back button on post page

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,12 +1,36 @@
 import "./Post.css";
 import PostCard from "@/components/PostCard.tsx";
 import CommentCard from "@/components/CommentCard.tsx";
+import {
+  ChevronLeftIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router";
 
 export default function Post() {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className="main-container">
+      <ChevronLeftIcon
+        className="md-icon back-icon"
+        onClick={() => navigate("/")}
+      />
       <PostCard showCommentsIcon={false} />
-      <CommentCard />
+      <div className="comment-container">
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+      </div>
+      <div className="comment-input-container">
+        <textarea
+          className="comment-input"
+          placeholder="Leave some comment..."
+        />
+        <PaperAirplaneIcon className="lg-icon" />
+      </div>
     </div>
   );
 }
