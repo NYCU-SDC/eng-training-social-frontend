177e8e7946e98e6789a99ace3d4686ecb053121b
yukicoder0509 <bendoris0509@gmail.com>
2025-10-07 19:05:52 +0800

    feat: create post page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import Home from "@/pages/Home";
 import CreatePost from "@/pages/CreatePost.tsx";
+import Post from "@/pages/Post.tsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/post/create" element={<CreatePost />} />
+      <Route path="/post/:id" element={<Post />} />
     </Routes>
   );
 }
