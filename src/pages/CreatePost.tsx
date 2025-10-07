0ac5267e64dbfe3b546ac193779e7b0dd59d7f43
yukicoder0509 <bendoris0509@gmail.com>
2025-10-07 18:58:50 +0800

    feat: add navigation on the back button in create post page

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,12 +1,15 @@
 import "./CreatePost.css";
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import Button from "@/components/Button.tsx";
+import { useNavigate } from "react-router";
 
 export default function CreatePost() {
+  const navigate = useNavigate();
+
   return (
     <div className="main-container">
       <div className="create-post-nav">
-        <ChevronLeftIcon className="md-icon" />
+        <ChevronLeftIcon className="md-icon" onClick={() => navigate("/")} />
         <h1>Create a New Post</h1>
         <Button>Publish</Button>
       </div>
