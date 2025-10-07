21ff16a037f22976553baba4e5f1a4caed9c4d4a
yukicoder0509 <bendoris0509@gmail.com>
2025-10-07 18:47:14 +0800

    feat: complete create post page

---
+++ src/pages/CreatePost.tsx
@@ -0,0 +1,17 @@
+import "./CreatePost.css";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import Button from "@/components/Button.tsx";
+
+export default function CreatePost() {
+  return (
+    <div className="page-container">
+      <div className="create-post-nav">
+        <ChevronLeftIcon className="md-icon" />
+        <h1>Create a New Post</h1>
+        <Button>Publish</Button>
+      </div>
+      <input placeholder="Title" />
+      <textarea placeholder="Say Something About It..." />
+    </div>
+  );
+}
