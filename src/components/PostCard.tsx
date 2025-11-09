7acd7ca8b88642adaaed59e69578051361c13a99
yukicoder0509 <bendoris0509@gmail.com>
2025-11-09 15:37:07 +0800

    feat: get posts data from backend. and add corresponding type, requests, and env

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,25 +5,23 @@ import {
   HandThumbDownIcon,
   ChatBubbleOvalLeftIcon,
 } from "@heroicons/react/24/outline";
+import { type Post } from "@/types/types";
 
 export default function PostCard({
+  post,
   showCommentsIcon = true,
 }: {
+  post: Post;
   showCommentsIcon?: boolean;
 }) {
   return (
     <div className="card-container">
       <div className="user-info">
         <UserCircleIcon className="lg-icon" />
-        <p>John Doe</p>
+        <p>{post.authorName}</p>
       </div>
-      <h1 className="card-title">Lorem ipsum dolor sit amet consectetur.</h1>
-      <p className="card-text">
-        Lorem ipsum dolor sit amet consectetur. Bibendum adipiscing metus dolor
-        diam in risus ut. Quis nam elit placerat egestas tellus rhoncus.
-        Facilisis pretium et nunc metus urna nisi a. Molestie consequat ornare
-        ac volutpat velit condimentum quis pellentesque.
-      </p>
+      <h1 className="card-title">{post.title}</h1>
+      <p className="card-text">{post.content}</p>
       <div className="reaction-container">
         <HandThumbUpIcon className="md-icon" />
         <HandThumbDownIcon className="md-icon" />
