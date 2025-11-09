6bea5803f1ac91a11c75168f246519b038581178
yukicoder0509 <bendoris0509@gmail.com>
2025-11-09 15:16:51 +0800

    feat: completw naigation, account, and user profile page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router";
 import Home from "@/pages/Home";
 import CreatePost from "@/pages/CreatePost.tsx";
 import Post from "@/pages/Post.tsx";
+import Account from "@/pages/Account.tsx";
+import UserProfile from "@/pages/UserProfile.tsx";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/post/create" element={<CreatePost />} />
       <Route path="/post/:id" element={<Post />} />
+      <Route path="/user/me" element={<Account />} />
+      <Route path="/user/:id" element={<UserProfile />} />
     </Routes>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,12 +4,12 @@ import {
   BellIcon as BellIconOutline,
   UserCircleIcon as UserCircleOutline,
 } from "@heroicons/react/24/outline";
-
 import {
   HomeIcon as HomeIconSolid,
   BellIcon as BellIconSolid,
   UserCircleIcon as UserCircleSolid,
 } from "@heroicons/react/24/solid";
+import { Link } from "react-router";
 
 export default function Navigation({
   page = "home",
@@ -18,21 +18,25 @@ export default function Navigation({
 }) {
   return (
     <div className="navigation-container">
-      {page == "home" ? (
-        <HomeIconSolid className="lg-icon" />
-      ) : (
-        <HomeIconOutline className="lg-icon" />
-      )}
+      <Link to="/" className="link">
+        {page == "home" ? (
+          <HomeIconSolid className="lg-icon" />
+        ) : (
+          <HomeIconOutline className="lg-icon" />
+        )}
+      </Link>
       {page == "notifications" ? (
         <BellIconSolid className="lg-icon" />
       ) : (
         <BellIconOutline className="lg-icon" />
       )}
-      {page == "account" ? (
-        <UserCircleSolid className="lg-icon" />
-      ) : (
-        <UserCircleOutline className="lg-icon" />
-      )}
+      <Link to="/user/me" className="link">
+        {page == "account" ? (
+          <UserCircleSolid className="lg-icon" />
+        ) : (
+          <UserCircleOutline className="lg-icon" />
+        )}
+      </Link>
     </div>
   );
 }
